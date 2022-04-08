64dcd4c6f6b9d52b882760e8e0690f77e43912f3
Natanael Matos <natanbmatos@gmail.com>
2022-04-08 14:30:36 -0300

    build: initial crud/solid structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { v4 as uuid } from "uuid";
 
 const app = express();
 
@@ -7,26 +6,51 @@ const port = 3456;
 
 app.use(express.json());
 
-const users = [];
-
-app.get("/users", (request, response) => {
-    return response.json(users);
-})
-
-app.post("/users", (request, response) => {
-    const { name, email } = request.body;
-
-    const user = {
-        name,
-        email,
-        id: uuid()
-    }
-
-    users.push(user);
-
-    return response.status(201).json(user);
-})
-
 app.listen(port, () => {
     console.log(`App running at port ${port}.`);
-}) 
+})
+
+// app.get("/users", (request, response) => {
+    //     return response.json(users);
+    // })
+
+// app.post("/users", (request, response) => {
+//     const { name, email } = request.body;
+
+//     const user = {
+//         name,
+//         email,
+//         id: uuid()
+//     }
+
+//     users.push(user);
+
+//     return response.status(201).json(user);
+// })
+
+// app.put("/users/:id", (request, response) => {
+//     const { id } = request.params;
+
+//     let user = users.filter((user) => user.id === id);
+
+//     if (!user.length) {
+//         return response.status(400).json({
+//             error: "User not found",
+//         })
+//     }
+
+//     const { name, email } = request.body;
+
+//     const updatedUser = {
+//         name,
+//         email,
+//         id
+//     }
+
+//     users.splice(user);
+//     user[0] = updatedUser;
+
+//     users.push(user[0]);
+
+//     return response.json(user[0]);
+// })
